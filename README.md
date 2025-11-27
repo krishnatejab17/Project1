ab002b42468fdb8fe8a11dce5359d0f16c3afdeb
Krishnatejab17 <17bdevops@gmail.com>
2025-11-27 12:16:15 +0000

    test commit 2

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Project1
 Using ECS Fargate deploy app
-Whenever code is pushed to GitHub, a Docker image should be created. T
-That Docker image should then be deployed to ECS Fargate. 
+Whenever code is pushed to GitHub, a Docker image should be created. 
+Docker image should be pushed to ECR
+That Docker image from ECR should then be deployed to ECS Fargate. 
 And the deployed service should have a load balancer. 
 When I hit the load balancer DNS, it should respond with 'welcome'."
--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_ecr_repository" "project1" {
   name = "project1-repo"
   image_tag_mutability = "IMMUTABLE"
@@ -7,4 +11,35 @@ resource "aws_ecr_repository" "project1" {
     
   }
 }
+
+
+#delete untagged images policy (https://www.linkedin.com/pulse/how-upload-docker-images-aws-ecr-using-terraform-hendrix-roa/)
+
+resource "aws_ecr_lifecycle_policy" "default_policy" {
+  repository = aws_ecr_repository.noiselesstech.name
+	
+
+	  policy = <<EOF
+	{
+	    "rules": [
+	        {
+	            "rulePriority": 1,
+	            "description": "Keep only the last ${var.untagged_images} untagged images.",
+	            "selection": {
+	                "tagStatus": "untagged",
+	                "countType": "imageCountMoreThan",
+	                "countNumber": ${var.untagged_images}
+	            },
+	            "action": {
+	                "type": "expire"
+	            }
+	        }
+	    ]
+	}
+	EOF
+	
+
+}
+
+
 
